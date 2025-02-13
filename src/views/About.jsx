614291878f23af271f066442e0407e38cc0c222b
Christina Vekri <christina.vek@gmail.com>
2025-02-13 16:09:00 +0100

    fixed small responsiveness bugs

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -15,9 +15,8 @@ const About = () => {
 
     const skills = [ "Javascript", "React", "Node.js", "Express", "MongoDB", "TypeScript", "HTML", "CSS", "Git" ];
 
-
     return (
-        <section id="about" className='min-h-screen font-source pt-14'>
+        <section id="about" className='min-h-screen font-source pt-20'>
           <div className='flex flex-col justify-center items-center gap-4 mt-8'>
               <h1 className='text-4xl md:text-7xl text-white text-center'>About me</h1>
               <hr className="w-3/4 md:w-1/2 border-0 h-0.5 bg-gradient-to-r from-transparent via-gray-400 to-transparent" />
@@ -30,7 +29,7 @@ const About = () => {
               transition={{ duration: 1, ease: "easeOut", delay: 0.2 }}
               className='w-full lg:w-1/3 flex flex-col items-center gap-4'>
                   <img src={aboutme} alt="aboutme" className='w-32 md:w-40 border border-white rounded-full p-2' />
-                  <p className='text-lg md:text-xl'>Driven by a passion for JavaScript, React, and the endless possibilities of web development, I thrive on creativity and problem-solving. Life-long learning is at the core of my journey, constantly pushing me to explore new challenges and technologies. Beyond coding, cooking, traveling, and watching series are some of the things that recharge my creativity and keep me inspired.</p>
+                  <p className='text-lg md:text-xl text-center md:text-left'>Driven by a passion for JavaScript, React, and the endless possibilities of web development, I thrive on creativity and problem-solving. Life-long learning is at the core of my journey, constantly pushing me to explore new challenges and technologies. Beyond coding, cooking, traveling, and watching series are some of the things that recharge my creativity and keep me inspired.</p>
               </motion.div>
           
               {/* Skill Set here */}
