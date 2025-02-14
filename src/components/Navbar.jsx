cb153eabac447455402ce2a3238a02868b08ae55
Christina Vekri <christina.vek@gmail.com>
2025-02-14 15:15:57 +0100

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-scroll';
 
 const Navbar = () => {
   return (
-    <nav className="navbar w-full bg-gray-800 bg-opacity-95 flex justify-end items-end font-montserrat shadow-lg p-2" style={{ zIndex: 1 }}>
+    <nav className="navbar w-full bg-gray-800 bg-opacity-95 flex justify-center items-center font-source shadow-lg p-4" style={{ zIndex: 1 }}>
       <ul className="flex space-x-8 text-white mr-8">
         {['Home', 'About', 'Projects', 'Contact'].map((item, index) => (
           <li key={index}>
